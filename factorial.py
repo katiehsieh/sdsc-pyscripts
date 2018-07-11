a6752c707816d08ac81ac33c127404762238c3ee
Katie Hsieh <katie@iteh.net>
2018-07-11 13:17:16 -0700

    recursion

--- factorial.py
+++ factorial.py
@@ -1,1 +1,21 @@
 print("Welcome to Recursion")
+
+# non recursive factorial function
+def nonRfactorial(n):
+    nonRecursiveAnswer = 1
+    while n >= 1:
+        nonRecursiveAnswer *= n
+        n -= 1
+    return nonRecursiveAnswer
+
+# recursive factorial function
+def factorial(n):
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+n = int(input("Enter an integer >= 0: "))
+
+print("Non Recursive: " + str(n) + "! = " + str(nonRfactorial(n)))
+print("Recursive: " + str(n) + "! = " + str(factorial(n)))
--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,13 @@
 print("Welcome to fibonacci")
+
+def fibonacci(n):
+    if n == 1 or n == 2:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+n = int(input("Enter an integer > 2: "))
+
+while n > 0:
+    print("Term " + str(n) + " in the fibonacci sequence is: " + str(fibonacci(n)))
+    n -= 1
